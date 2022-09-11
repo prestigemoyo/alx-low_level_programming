85c8e6c31432f7cb8bd8de272ae68220256d84b6
prestigemoyo <prestigecmoyo@gmail.com>
2022-09-11 13:12:42 -0700

    aa

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main -  Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+   puts("\"Programming is like building a multilin   gual puzzle");
+   return (0);
+
+} 

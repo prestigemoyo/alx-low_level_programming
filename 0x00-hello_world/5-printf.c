fe3ce9eab897420b4747f16f562839beb9c668e8
prestigemoyo <prestigecmoyo@gmail.com>
2022-09-11 15:24:05 -0700

    aaa

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main-Entry point
- * Return always 0
+ * main - Entry point
+ *
+ * Return: always 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of work\n):
+printf("with proper grammar, but the outcome is a piece of work\n");
 return (0);
-} 
+}
 

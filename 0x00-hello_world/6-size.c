1ebdb8a66fecb96f1bd07b4a4b1b81803d198242
prestigemoyo <prestigecmoyo@gmail.com>
2022-09-12 04:47:44 -0700

    aaa

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,16 @@
 
 int main(void)
 {
-char c;
-int i;
-long int li;
-long long int lli;
-float f;
+char charType;
+int intType;
+long int longIntType;
+long long int longLongType;
+float floatType;
 
-printf("Size of a char:%zu byte(s)\n", sizeof(c));
-printf("Size of an int: %zu byte(s)\n", sizeof(i));
-printf("Size of a long int: %zu byte(s)\n", sizeof(li));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(lli));
-printf("Size of a float: %zu byte(s)\n", sizeof(f));
+printf("Size of a char:%zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }

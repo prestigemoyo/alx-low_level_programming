6f37ae5edd50f9a97b80cea6fbd73e6db991e201
prestigemoyo <prestigecmoyo@gmail.com>
2022-09-12 18:53:36 -0700

    aaa

---
+++ 0x00-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main-This is the entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char low
+for (low = 'z'; low <= 'z'; low++);
+{
+putchar(x);
+putchar('\n');
+}
+return (0);
+}

81d2e2a07ebf7ac3835498bf751cae90c9c7be4d
prestigemoyo <prestigecmoyo@gmail.com>
2022-09-13 17:43:27 -0700

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#iclude "main.h"
+
+/**
+ * print_alphabet_x10 - function to print alphabet 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+
+{
+	char c, i;
+
+	for (c = 'a'; c <= 'z'; ctt)
+	{
+		_putchar(c);
+	}
+	_putchar('\n')
+}
+
+

5e51c81d7775c06272c904c01471983b90ce8b0c
prestigemoyo <prestigecmoyo@gmail.com>
2022-09-14 07:33:36 -0700

    fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,16 +15,16 @@ void jack_bauer(void)
 	int hours = 0;
 	int minutes = 0;
 	int hours_remainder;
-	int minutes_remainder;
+	int mins_remainder;
 
 	while (hours <= 23)
 	{
-	while (minutes = <= 59)
+	while (minutes <= 59)
 	{
 	mins_remainder = minutes % 10;
 	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
-	_putchar(hour_remainder + '0');
+	_putchar(hours_remainder + '0');
 	_putchar(':');
 	_putchar(minutes / 10 + '0');
 	_putchar(mins_remainder + '0');

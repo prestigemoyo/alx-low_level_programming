56dfa9f9f1b0ca452082ac085ada501faa2cb3d9
prestigemoyo <prestigecmoyo@gmail.com>
2022-09-14 10:38:24 -0700

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,14 +16,18 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(' ');
+
 			prod = num * mult;
 
-			if (prod <= 9)
+			if (prod 6 <= 9)
 				_putchar(' ');
 			else
-				_putchar(prod / 10				+ '0');
+				_putchar((prod / 10) + '0);
+
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
+
 	}
 }
 

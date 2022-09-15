a1952904de84fc9b1ac70d9970ff2778873b9351
prestigemoyo <prestigecmoyo@gmail.com>
2022-09-14 19:34:06 -0700

    tired ND SICK

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,15 @@ void print_to_98(int n)
 	while (n > 98)
 	printf("%d\n", n--);
 
-	printf("%d, n);
+	printf("%d", n);
 
 	}
 	else
 	{
 	while (n < 98)
-	printf("%d"," n++);
+	printf("%d", "," n++);
 	printf("%d\n", n);
+
 	}
 }
 

def4378b95b356ce387675f0d2b1fd264edf90c6
prestigemoyo <prestigecmoyo@gmail.com>
2022-09-14 18:40:56 -0700

    tired and sick

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,19 +7,16 @@
  */
 int main(void)
 {
-	unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+while (n3 <= 4000000)
+{
+n3 = n1 + n2;
+n1 = n2;
+n2 = n3;
 
-	while (n3 <= 4000000)
-	{
-		n3 = n1 + n2;
-		n1 = n2;
-		n2 = n3;
-
-		if ((n1 % 2) == 0)
-		sum += n1;
-	}
-	printf("%1u\n", sum);
-
-	return (0);
-
+if ((n1 % 2) == 0)
+sum += n1;
+}
+printf("%1d\n", sum);
+return (0);
 }

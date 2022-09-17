e965772ec76038308a0bb27b8e2b78adc7a04e85
prestigemoyo <prestigecmoyo@gmail.com>
2022-09-17 14:34:14 -0700

    fix fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,24 +9,27 @@
 
 void print_diagonal(int n)
 {
-int len, space;
+	int len, space;
 
-if (n > 0)
-{
-for (len = 0; len < n; len++)
-{
-for (space = o; space < len; space++)
-{
-putchar(' ');
-}
-putchar('\\');
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
+			{
+				putchar(' ');
+			}
 
-if (len == (n - 1))
-{
-continue;
-}
-putchar('\n');
-}
-}
-putchar('\n');
+
+			putchar('\\');
+
+
+			if (len == (n - 1))
+			{
+				continue;
+			}
+			putchar('\n');
+		}
+	}
+	putchar('\n');
 }

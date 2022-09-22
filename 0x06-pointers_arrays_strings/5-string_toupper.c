b8cf8a270fc998aaa5bb0a313292ab11e78182e0
prestigemoyo <prestigecmoyo@gmail.com>
2022-09-22 07:34:10 -0700

    change string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* string_toupper -> changes all lowercase letters of a string to uppercase.
+* @x: string parameter
+* Return: string
+*/
+char *string_toupper(char *x)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+			x[i] = x[i] - 32;
+		i++;
+
+	}
+	return (x);
+}

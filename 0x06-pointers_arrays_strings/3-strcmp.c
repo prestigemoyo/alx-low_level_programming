59bf7e40088db63561a1be81ad9b3648057f89d2
prestigemoyo <prestigecmoyo@gmail.com>
2022-09-22 07:12:55 -0700

    compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strcmp -> function that compares two strings
+* @s1: first parameter
+* @s2: second parameter
+* Return: return an integer
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int comp;
+
+	comp = strcmp(s1, s2);
+	return (comp);
+
+}
